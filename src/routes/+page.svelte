c3bf96e72225600880ee1b39051daacfec3d59de
Dominik Hofer <hfrdmnk@gmail.com>
2023-09-26 15:50:27 +0200

    Added shadcn/ui & tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Pelish</h1>
+<p>The (Simple) Personal Link Shortener.</p>
