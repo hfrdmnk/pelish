3266edf3bae76f5c662191d4217e757337f63b07
Dominik Hofer <hfrdmnk@gmail.com>
2023-11-10 11:34:17 +0900

    Very basic redirect functionality

--- src/routes/[slug]/+server.ts
+++ src/routes/[slug]/+server.ts
@@ -1,9 +1,25 @@
+import { redirect, error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import type { Shorturl } from '$lib/databaseItem.types';
 
 export const GET: RequestHandler = async (event) => {
 	const {
 		params: { slug },
 		locals: { supabase }
 	} = event;
-	return new Response(JSON.stringify({ slug, supabase }));
+
+	// fetch all relevant shorturls
+	const { data: supabaseData, error: supabaseError } = await supabase
+		.from('links')
+		.select('*')
+		.eq('short_url', slug)
+		.single();
+
+	if (supabaseError) {
+		throw error(500, 'There was unfortunately an error fetching your shorturls');
+	}
+
+	const shorturl: Shorturl = supabaseData;
+
+	throw redirect(303, shorturl.redirect_url);
 };
