e7dea70bda96064cde2a40b746daf1ae0f2bac34
Dominik Hofer <hfrdmnk@gmail.com>
2023-11-13 21:02:01 +0100

    Collect basic request data

--- src/routes/[slug]/+server.ts
+++ src/routes/[slug]/+server.ts
@@ -5,12 +5,36 @@
 import { redirect, error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import type { Shorturl } from '$lib/databaseItem.types';
+import { UAParser } from 'ua-parser-js';
+import isbot from 'isbot';
 
 export const GET: RequestHandler = async (event) => {
 	const {
 		params: { slug },
 		locals: { supabase }
 	} = event;
+
+	const requestData = {
+		userAgent: {},
+		location: {},
+		datetime: new Date()
+	};
+
+	const userAgent = event.request.headers.get('user-agent');
+	if (userAgent) {
+		// check if user is a bot
+		if (isbot(userAgent)) {
+			throw error(403, 'Bots have to wait outside, sorry 🤖');
+		}
+
+		// parse user agent
+		const uaData = UAParser(userAgent);
+		requestData.userAgent = {
+			browser: uaData.browser,
+			device: uaData.device,
+			os: uaData.os
+		};
+	}
 
 	// fetch all relevant shorturls
 	const { data: supabaseData, error: supabaseError } = await supabase
